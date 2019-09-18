761ec076f8a51e0b118d51fde1f7f9697f2053c6
Daw-Ran Liou <dawran6@gmail.com>
2019-09-18 08:44:25 -0700

    Init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to emoji
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/emoji/core_test.clj
@@ -0,0 +1,7 @@
+(ns emoji.core-test
+  (:require [clojure.test :refer :all]
+            [emoji.core :as emoji]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
