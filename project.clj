5b792682b294dcbd1a952bf262cc255af5d4577f
Daw-Ran Liou <dawran6@gmail.com>
2019-09-20 09:28:51 -0700

    Use project.clj for library development

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject emoji "0.1.3-SNAPSHOT"
+  :description "Emoji for Clojure"
+  :url "http://github.com/dawran6/emoji"
+  :license {:name "MIT"
+            :url  "https://github.com/dawran6/emoji/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "0.2.6"]]
+  :repl-options {:init-ns user})
